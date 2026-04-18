80a306a8af539c21061ca9c015647abc5185d72a
Stanislav Gospodinov <shonito44@gmail.com>
2026-04-18 16:22:16 +0300

    Setup the project and firebase config

---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { firebaseConfig } from './firebase.config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
+};
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAn6IR5C5aXgTuU4YZ3iaB4dBph57s-2BA',
+  authDomain: 'game-blog-62355.firebaseapp.com',
+  projectId: 'game-blog-62355',
+  storageBucket: 'game-blog-62355.firebasestorage.app',
+  messagingSenderId: '826257492486',
+  appId: '1:826257492486:web:f8ad6e42ca16cd68429187',
+};
+
+const app = initializeApp(firebaseConfig);
